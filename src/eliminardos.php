50ca94a0dde84c846b02879f352ddc1aa5905117
fde_analista01 <analista01@fde.pe>
2026-02-12 18:06:04 -0500

    se agrega html

--- src/eliminardos.php
+++ src/eliminardos.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
 $variable = "hola";
 echo "se cambia de mensaje";
@@ -6,3 +14,6 @@ echo "se agrega una linea mas";
 echo "se agrega una linea mas en la ramasiete";
 $oAuto = "{['a', 'b', 'g'], [1,5,8,6]}";
 echo $oAuto;
+?>    
+</body>
+</html>
