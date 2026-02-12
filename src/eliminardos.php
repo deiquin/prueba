0274f594219c545cca546939dd65cb828e151aac
fde_analista01 <analista01@fde.pe>
2026-02-12 17:17:06 -0500

    se agrega objeto

--- src/eliminardos.php
+++ src/eliminardos.php
@@ -4,3 +4,5 @@ echo "se cambia de mensaje";
 echo "uno mas";
 echo "se agrega una linea mas";
 echo "se agrega una linea mas en la ramasiete";
+$oAuto = "{['a', 'b', 'g'], [1,5,8,6]}";
+echo $oAuto;
